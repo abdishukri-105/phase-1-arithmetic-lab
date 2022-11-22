0d7157d22ef77cd787a23ffc122b0920ce883426
abdishukri <abdishukrimohamed105@gmail.com>
2022-11-22 15:26:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+
+let num1 = 2
+let num2 = 31
+
+let multiply = num1 * num2
+
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num4 = 14
+let num3 = 5
+let mod = num4 % num3
+
+let max = Math.max(1,4,6,7,20)
